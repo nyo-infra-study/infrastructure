1d16e037ee65fd0c92ccda7ebef85494339173b1
yonatan-nyo <yonatan.njoto@gmail.comm>
2026-02-21 23:05:35 +0700

    fix(dns): patch CoreDNS to bypass Colima UDP Egress bug by forwarding to Google DNS

--- run.sh
+++ run.sh
@@ -113,6 +113,13 @@ run "k3d cluster create dev \
   --k3s-arg '--flannel-backend=none@server:0' \
   --k3s-arg '--disable-network-policy@server:0'"
 
+log_step "Patching CoreDNS (Bypass Colima DNS)"
+# Colima's DNS proxy (192.168.5.2) drops SNATed UDP packets from Cilium.
+# We patch CoreDNS to forward directly to 8.8.8.8 to ensure cluster egress DNS works.
+run "kubectl get cm coredns -n kube-system -o yaml | sed 's/forward . \/etc\/resolv.conf/forward . 8.8.8.8/' | kubectl apply -f -"
+run "kubectl rollout restart deployment coredns -n kube-system"
+run "kubectl rollout status deployment/coredns -n kube-system --timeout=60s"
+
 log_step "Adding Helm Repositories"
 run "helm repo add argo https://argoproj.github.io/argo-helm"
 run "helm repo add cilium https://helm.cilium.io/"
